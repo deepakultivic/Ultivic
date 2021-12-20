c95b55be9e106b67c6f9b8a51aa98003d02ff086
deepalultivic <deepak.chauhan@ultivic.com>
2021-12-20 19:17:42 +0530

    app development

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -260,14 +260,14 @@ function Home() {
               </div> */}
             <div className="row align-items-center">
 
-              <div class="col-lg-6 col-md-6 col-12 ">
-                <div class="section-title ml-lg-5">
+              <div className="col-lg-6 col-md-6 col-12 ">
+                <div className="section-title ml-lg-5">
                   <h6 className="sub_heading placement">Our Expertise</h6>
-                  <h4 class="title mb-4">
+                  <h4 className="title mb-4">
                     We Provide IT solutions <br /> that performs.
 
                   </h4>
-                  <p class="text-muted mb-0">You share your Ideas and We Make It Real. We ensure the true worth of your valuable time and money you invested on us by delivering the perfect solutions that meet client’s needs, expectations and budget.</p>
+                  <p className="text-muted mb-0">You share your Ideas and We Make It Real. We ensure the true worth of your valuable time and money you invested on us by delivering the perfect solutions that meet client’s needs, expectations and budget.</p>
                 <h5 className="my-3">
                   Our Technologies
                 </h5>
