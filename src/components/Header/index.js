cbfdfd0725f4eb317cdc7fd9360ced4502ad512e
deepalultivic <deepak.chauhan@ultivic.com>
2021-12-21 18:51:04 +0530

    drop down menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-import { Link,NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 import './style.css';
 
-const  Header = () => {
+const Header = () => {
   const [scrolled, setScrolled] = React.useState(false);
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -21,47 +21,63 @@ const  Header = () => {
   if (scrolled) {
     navbarClasses.push("scrolled");
   }
- 
+
   return (
 
-      <header className={navbarClasses.join(" ")}>
-       
-          <nav className="navbar navbar-expand-lg navbar-light ">
-            <NavLink to="/" className="navbar-brand">   <img src="/assets/header/logo-white.png" height="100" alt="CoolBrand" /></NavLink>
-          
-            <button
-              type="button"
-              className="navbar-toggler ml-auto"
-              data-toggle="collapse"
-              data-target="#navbarCollapse"
-            >
-              <span className="fa fa-bars"></span>
-            </button>
+    <header className={navbarClasses.join(" ")}>
 
-            <div className="collapse navbar-collapse " id="navbarCollapse">
-              <div className="navbar-nav ml-auto">
-                <NavLink activeClassName="active" to="/about" className="nav-item nav-link ">About</NavLink>
-                <NavLink activeClassName="active" to="/portfolio" className="nav-item nav-link ">Portfolio</NavLink>
-                <li  className="nav-item nav-link ">
-                Services <i className="	fa fa-angle-down"></i>
-                <ul className="drop_menu">
-                  <li><NavLink  activeClassName="active" to="/web">Web Development</NavLink></li>
-                  <li><NavLink  activeClassName="active" to="/graphic">Graphic Design</NavLink></li>
-                  <li><NavLink  activeClassName="active" to="/app">App Development</NavLink></li>
-                </ul>
-               </li>
-               <NavLink  activeClassName="active" to="/blogs" className="nav-item nav-link">  Blog</NavLink>
-               <NavLink  activeClassName="active" to="/career" className="nav-item nav-link">Career</NavLink>
-               <NavLink  activeClassName="active" to="/contact" className="nav-item nav-link">Contact</NavLink>
-               
-              </div>
-             
-             
-            </div>
-          </nav>
-     
-      </header>
-   
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <NavLink to="/" className="navbar-brand">   <img src="/assets/header/logo-white.png" height="100" alt="CoolBrand" /></NavLink>
+
+        <button
+          type="button"
+          className="navbar-toggler ml-auto"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span className="fa fa-bars"></span>
+        </button>
+
+        <div className="collapse navbar-collapse " id="navbarCollapse">
+          <div className="navbar-nav ml-auto">
+            <NavLink activeClassName="active" to="/about" className="nav-item nav-link ">About</NavLink>
+            <NavLink activeClassName="active" to="/portfolio" className="nav-item nav-link ">Portfolio</NavLink>
+            <li className="nav-item nav-link mobile_dropdown">
+              Services <i className="	fa fa-angle-down"></i>
+              <ul className="drop_menu ">
+                <li><NavLink activeClassName="active" to="/web">Web Development</NavLink></li>
+                <li><NavLink activeClassName="active" to="/graphic">Graphic Design</NavLink></li>
+                <li><NavLink activeClassName="active" to="/app">App Development</NavLink></li>
+              </ul>
+            </li>
+            <li className="nav-item nav-link desktop_dropdown">
+              Services <i className="	fa fa-angle-down"></i>
+              <ul className="drop_menu">
+                <li><NavLink activeClassName="active" to="/web"><div className="menu_image">
+                  <img src="/assets/header/webdevelopment.jpg" alt="" />
+                  <p> Web Development</p>
+                </div></NavLink></li>
+                <li><NavLink activeClassName="active" to="/graphic"><div className="menu_image">
+                  <img src="/assets/header/graphics.jpg" alt="" />
+                  <p>Graphic Design</p></div></NavLink></li>
+                <li><NavLink activeClassName="active" to="/app"><div className="menu_image">
+                  <img src="/assets/header/appdevelopment.jpg" alt="" />
+                  <p>App Development</p>
+                </div></NavLink></li>
+              </ul>
+            </li>
+            <NavLink activeClassName="active" to="/blogs" className="nav-item nav-link">  Blog</NavLink>
+            <NavLink activeClassName="active" to="/career" className="nav-item nav-link">Career</NavLink>
+            <NavLink activeClassName="active" to="/contact" className="nav-item nav-link">Contact</NavLink>
+
+          </div>
+
+
+        </div>
+      </nav>
+
+    </header>
+
   );
 };
 
